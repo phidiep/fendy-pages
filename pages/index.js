5bcb7250db8de6f60f54c65d859042995228f717
phidiep <longphi11a5@gmail.com>
2022-06-29 21:28:27 +0900

    add social, certificate

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,31 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Link from 'next/link'
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import { Card, Row} from "antd";
+import Link from "next/link";
 
 export default function Index() {
+  const { Meta } = Card;
+  const info = [
+    {
+      link: "blog",
+      img: "https://drscdn.500px.org/photo/1013482204/m%3D900/v2?sig=e37fc4aec575af82558647188deefa33d3c05f15615694415253da9b4a35693b",
+      imgAlt: "Smiling girl by fendy studio on 500px.com",
+      title: "Blog",
+      description: "My blog",
+    },
+    {
+      link: "portfolio",
+      img: "https://drscdn.500px.org/photo/1047833008/m%3D900/v2?sig=33fb40a54359a4c93cb8e5e7d1a59bc3ec92635a873bca310ab65d3d0a59fba4",
+      imgAlt: "kimono in snow by fendy studio on 500px.com",
+      title: "Portfolio",
+      description: "My portfolio",
+    },
+  ];
   return (
     <>
       <Layout>
@@ -15,16 +33,23 @@ export default function Index() {
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
         <Container>
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
-        <Link href="/portfolio">
-          <a>portfolio</a>
-        </Link>
-
+        <Row justify="center" > 
+        <div style={{display:"flex"}}>
+          {info.map((item) => (
+            <Link href={`/${item.link}`}>
+              <Card 
+                hoverable
+                style={{ width: 240 , margin:20}}
+                cover={<img src={item.img} alt={item.alt} />}
+              >
+                <Meta title={item.title} description={item.description} />
+              </Card>
+            </Link>
+          ))}
+          </div>
+          </Row>
         </Container>
       </Layout>
     </>
-  )
+  );
 }
-
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,22 +1,28 @@
-import { Timeline, Avatar, Layout, Col, Row, Typography , Image } from "antd";
-// import Image from 'next/image'
-// import avatar from '/assets/portfolio/avatar.jpg'
+import { Timeline, Avatar, Layout, Col, Row, Typography, Tag } from "antd";
+import {
+  FacebookOutlined,
+  LinkedinOutlined,
+  YoutubeOutlined,
+} from "@ant-design/icons";
+import Link from "next/link";
+import Title from "antd/lib/skeleton/Title";
 const { Content } = Layout;
 const { Text, Paragraph } = Typography;
 export default function Portfolio() {
   const timelineData = [
     {
-      key : 1,
+      key: 1,
       role: "Android Developer",
       companyName: "ITS LAB",
       time: "MAR 2017 - FEB 2018 - Ho Chi Minh, Viet Nam",
       detail: `Create an android app that provides real-time traffic
       information to user and control by voice. Responsibility :
       Android app coding, aws services integrating.`,
-      techStack:"Java, Gitlab, Git, AWS, Android, Linux, RESTful API, MongoDB, gitlab, Python, Django",
+      techStack:
+        "Java, Gitlab, Git, AWS, Android, Linux, RESTful API, MongoDB, gitlab, Python, Django",
     },
     {
-      key : 2,
+      key: 2,
       role: "Full Stack Engineer .NET",
       companyName: "SORIMACHI VIET NAM",
       time: "MAR 2017 - JUL 2019 - Ho Chi Minh, Viet Nam (2 years 6 month)",
@@ -24,10 +30,10 @@ export default function Portfolio() {
       a member and a few months as a team lead. Responsibility :
       coding, testing, database manager , server manager, member
       manager, document writing. `,
-      techStack:"C#, .NET , HTML, CSS, jQuery, Bootstrap , SVN, SQL Server"
+      techStack: "C#, .NET , HTML, CSS, jQuery, Bootstrap , SVN, SQL Server",
     },
     {
-      key : 3,
+      key: 3,
       role: "System integration Engineer",
       companyName: "AXAS CO.,LTD.",
       time: "AUG 2019 - Present - Tokyo, Japan",
@@ -42,12 +48,27 @@ export default function Portfolio() {
       techStack: "",
     },
   ];
+  const cert = [
+    {
+      name : "TOEIC 675",
+      time : "2017-01"
+    },
+    {
+      name : "JLPT N2",
+      time : "2020-12"
+    },
+    {
+      name : "AWS Architecture Associate ",
+      time : "2022-04"
+    },
+  ]
+
 
   return (
     <>
       <Layout style={{ minHeight: "100vh", backgroundColor: "white" }}>
         <Content>
-          <Row style={{ minHeight: "20vh" }}></Row>
+          <Row style={{ minHeight: "20vh", maxwidth:700 }}></Row>
           <Row justify="center" style={{ minHeight: "20vh" }}>
             <Col span={4} style={{ textAlign: "center" }}>
               <Avatar size={200} src="/assets/portfolio/avatar.jpg" />
@@ -61,26 +82,68 @@ export default function Portfolio() {
                 I’m a fullstack engineer and try to moving my skill to cloud
                 especially AWS
               </Typography.Title>
+              <Link href="https://www.youtube.com/channel/UCCNjyfVOMCDWOlpqqIIT4lQ">
+              <Tag
+                icon={<YoutubeOutlined style={{ verticalAlign: "0.125em" }} />}
+                color="#cd201f"
+              >
+                Youtube
+              </Tag>
+              </Link>
+              <Tag
+                icon={<FacebookOutlined style={{ verticalAlign: "0.125em" }} />}
+                color="#3b5999"
+              >
+                Facebook
+              </Tag>
+              <Link href="https://www.youtube.com/channel/UCCNjyfVOMCDWOlpqqIIT4lQ">
+              <Tag
+                icon={<LinkedinOutlined style={{ verticalAlign: "0.125em" }} />}
+                color="#55acee"
+              >
+                LinkedIn
+              </Tag>
+              </Link>
             </Col>
           </Row>
           <Row justify="center" style={{ minHeight: "80vh" }}>
             <Col span={4}></Col>
             <Col span={8}>
-              <Timeline pending="Pending..." reverse={"true"}>
+            <Typography.Title level={2}>Work</Typography.Title>
+              <Timeline reverse={"true"}>
                 {timelineData.map((data) => (
                   <Timeline.Item key={data.key}>
                     <Typography.Title level={5}>{data.role}</Typography.Title>
-                    <Typography.Title level={5} style={{ fontWeight: "lighter" }}>
+                    <Typography.Title
+                      level={5}
+                      style={{ fontWeight: "lighter" }}
+                    >
                       {data.companyName}
-                    </Typography.Title>
-                    <Text style={{ fontWeight: "lighter" }} italic></Text>
+                    </Typography.Title> 
+                    <Text style={{ fontWeight: "lighter" }} italic>{data.time}</Text>
                     <Paragraph
-                      ellipsis={{ rows: 4, expandable: true, symbol: "more" }}
+                      ellipsis={{ rows: 5, expandable: true, symbol: "more" }}
                       style={{ width: 700 }}
-                    >{data.detail}</Paragraph>
+                    >
+                      {data.detail}
+                    </Paragraph>
                     <Paragraph italic style={{ fontWeight: "lighter" }}>
                       TechStack : {data.techStack}
                     </Paragraph>
+                  </Timeline.Item>
+                ))}
+              </Timeline>
+            </Col>
+          </Row>
+          <Row justify="center" style={{ minHeight: "80vh" }}>
+            <Col span={4}></Col>
+            <Col span={8}>
+            <Typography.Title level={2}>Certification</Typography.Title>
+              <Timeline reverse={"true"}>
+                {cert.map((data,index) => (
+                  <Timeline.Item key={index}>
+                    <Typography.Title level={5}>{data.name}</Typography.Title>
+                    <Text style={{ fontWeight: "lighter" }} italic>{data.time}</Text>
                   </Timeline.Item>
                 ))}
               </Timeline>
